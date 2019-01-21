d4c3635182c9b4defed7db12425d0d36abe78371
今竹太樹 <imamura201611@gmail.com>
2019-01-21 11:49:14 +0800

    ログイン可能ログイン後の画面に(サインイン機能付加) 結びつけた

--- signin.php
+++ signin.php
@@ -1,5 +1,9 @@
 <?php
 
+session_start();
+require('dbconnect.php');
+
+
  $errors = [];
 
  if (!empty($_POST)){
@@ -9,12 +13,45 @@
         //正常系
         //両方入力
         //データベースとの照合処理
-    }else {
+
+        $sql = 'SELECT * FROM `users` WHERE `email` = ?';
+//1.入力されたメールアドレスと一致する登録データを1件DBから所得
+
+        $data = [$email];
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute($data);
+        // $recordはDBの１レコードにあたいする
+        // 形式は連想配列
+        // キーはカラムに依存する
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    //SELECT文の実行結果がある場合は連想配列
+    //結果がない場合はfalseが入る
+        if ($record == false) {
+        $errors['signin'] = 'failed';
+        }
+
+        //2.パスワード照合
+        // password_verify(文字列,ハッシュ化された文字列)
+        // 指定した2つの文字列が合致する場合true
+        if(password_verify($password, $record['password'])){
+        //認証成功
+
+        //3,セッションにIDを格納
+            $SESSION['49_LearnSNS']['id'] = $record['id'];
+
+        //4,タイムライン画面に遷移
+            header('Location: timeline.php');
+            exit();
+
+        }else {
+        //認証失敗
+            $errors['signin'] = 'failed';
+        }
+    }else{
         $errors['signin'] = 'blank';
     }
-
- }
-
+}
   ?>
 
 <?php include('layouts/header.php'); ?>
@@ -27,9 +64,12 @@
                     <div class="form-group">
                         <label for="email">メールアドレス</label>
                         <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
-                            <?php if(isset($errors['singin']) && $errors['singin'] == 'blank'): ?>
+                            <?php if(isset($errors['signin']) && $errors['signin'] == 'blank'): ?>
                             <p class = "text-danger">メールアドレスとパスワードを正しく入力してください</p>
                         <?php endif;?>
+                        <?php if(isset($errors['signin']) && $errors['signin'] == 'failed'): ?>
+                            <p class="text-danger">サインインに失敗しました</p>
+                        <?php endif; ?>    
                     </div>
                     <div class="form-group">
                         <label for="password">パスワード</label>
